819b50720965bafb66a05a9063d003e524faf42f
piyushrai7271 <piyushrai7271@gmail.com>
2024-03-27 18:22:50 +0530

    worked on react-toast package for clip board

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,8 +1,13 @@
 import { useRef, useState, useEffect } from "react";
+import React from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 
 const Manager = () => {
   const ref = useRef();
+  const passwordRef = useRef();
   const [form, setForm] = useState({ site: "", username: "", password: "" });
   const [passwordArray, setPasswordArray] = useState([]);
 
@@ -13,12 +18,28 @@ const Manager = () => {
     }
   }, []);
 
+  const copyText = (text) =>{
+    toast('🦄 Copied to clipboard', {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      });
+    navigator.clipboard.writeText(text)
+  }
+
   const showPassword = () => {
-    alert("show the Password");
+    passwordRef.current.type = "text";
     if (ref.current.src.includes("icon/hidden.png")) {
       ref.current.src = "icon/view.png";
+      passwordRef.current.type = "text";
     } else {
       ref.current.src = "icon/hidden.png";
+      passwordRef.current.type = "password";
     }
   };
 
@@ -34,6 +55,20 @@ const Manager = () => {
 
   return (
     <>
+    <ToastContainer
+position="top-right"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="light"
+/>
+{/* Same as */}
+<ToastContainer />
       <div
         className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),
        linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"
@@ -74,11 +109,12 @@ const Manager = () => {
             />
             <div className="relative">
               <input
+                ref={passwordRef}
                 value={form.password}
                 className="rounded-full border border-green-500 w-full p-4 py-1"
                 placeholder="Enter Password"
                 onChange={handleChange}
-                type="text"
+                type="password"
                 name="password"
                 id=""
               />
@@ -110,25 +146,75 @@ const Manager = () => {
         <div className="password">
           <h2 className="font-bold text-2xl py-4">Your Password</h2>
           {passwordArray.lenght === 0 && <div>No Passwords to show </div>}
-          {passwordArray.lenght != 0 &&
-          <table className="table-auto w-full rounded-md overflow-hidden">
-            <thead className="bg-green-800 text-white">
-              <tr>
-                <th className="py-2">Site</th>
-                <th className="py-2">Username</th>
-                <th className="py-2">Password</th>
-              </tr>
-            </thead>
-            <tbody className='bg-green-100'>
-              {passwordArray.map((item,index)=>{
-                 return  <tr key={index}>
-                  <td className=" py-2 border border-white text-center w-32"><a href={item.site} target="_blank">{item.site}</a></td>
-                  <td className=" py-2 border border-white text-center w-32">{item.username}</td>
-                  <td className=" py-2 border border-white text-center w-32">{item.password}</td>
-                </tr> 
-              })}
-            </tbody>
-          </table>}
+          {passwordArray.lenght != 0 && (
+            <table className="table-auto w-full rounded-md overflow-hidden">
+              <thead className="bg-green-800 text-white">
+                <tr>
+                  <th className="py-2">Site</th>
+                  <th className="py-2">Username</th>
+                  <th className="py-2">Password</th>
+                </tr>
+              </thead>
+              <tbody className="bg-green-100">
+                {passwordArray.map((item, index) => {
+                  return (
+                    <tr key={index}>
+                      <td className=" py-2 border border-white text-center">
+                        <div className="flex items-center justify-center">
+                        <a href={item.site} target="_blank">
+                          {item.site}
+                        </a>
+                        <div className="lordiconcopy  size-7 cursor-pointer" onClick={()=>{copyText(item.site)}}>
+                          <lord-icon
+                            style={{
+                              width: "25px",
+                              height: "25px",
+                              "paddingTop": "3px",
+                            }}
+                            src="https://cdn.lordicon.com/tzdwqlbp.json"
+                            trigger="hover"
+                          ></lord-icon>
+                        </div>
+                        </div>
+                      </td>
+                      <td className=" py-2 border border-white text-center w-32">
+                        <div className="flex items-center justify-cente">
+                        <span>{item.username}</span>
+                        <div className="lordiconcopy  size-7 cursor-pointer" onClick={()=>{copyText(item.username)}}>
+                          <lord-icon
+                            style={{
+                              width: "25px",
+                              height: "25px",
+                              "paddingTop": "3px",
+                            }}
+                            src="https://cdn.lordicon.com/tzdwqlbp.json"
+                            trigger="hover"
+                          ></lord-icon>
+                        </div>
+                        </div>
+                      </td>
+                      <td className="  py-2 border border-white text-center w-32">
+                        <div className="flex items-center justify-cente">
+                        <span>{item.password}</span>
+                        <div className="lordiconcopy size-7 cursor-pointer" onClick={()=>{copyText(item.password)}}>
+                          <lord-icon
+                            style={{
+                              width: "25px",
+                              height: "25px",
+                              "paddingTop": "3px",
+                            }}
+                            src="https://cdn.lordicon.com/tzdwqlbp.json"
+                            trigger="hover"
+                          ></lord-icon>
+                        </div>
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          )}
         </div>
       </div>
     </>
